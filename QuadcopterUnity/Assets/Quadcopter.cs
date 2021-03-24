a3a194670b743f217b5f8eae5afde75ed55e9bbb
James <jamesscn@gmail.com>
2021-03-24 00:03:10 -0100

    Better organization of code

--- QuadcopterUnity/Assets/Quadcopter.cs
+++ QuadcopterUnity/Assets/Quadcopter.cs
@@ -1,57 +1,66 @@
 using UnityEngine;
 
-public class Motor {
-
-	private float TorqueConstant, MotorConstant, Resistance;
-
-	public Motor(float torqueConstant, float motorConstant, float resistance) {
-		TorqueConstant = torqueConstant;
-		MotorConstant = motorConstant;
-		Resistance = resistance;
-	}
-
-	public float CalculateTorque(float voltage, float angularVelocity) {
-		if(voltage <= 0.0F) {
-			return 0.0F;
-		}
-		return TorqueConstant / Resistance * (voltage - MotorConstant * angularVelocity);
-	}
-
-}
-
 public class Quadcopter : MonoBehaviour {
 
-	//Front Left, Front Right, Back Right, Back Left
-	public Rigidbody[] Blades;
+	//This is an array of rigidbodies of all of the rotors of the quadcopter
+	//The order of the rotors is Front Left, Front Right, Back Right and Back Left
+	public Rigidbody[] Rotors;
+
+	//The rigidbody for the frame or chassis of the quadcopter is stored in this variable
 	public Rigidbody Frame;
 
+	//This variable is the slider for the voltage difference
 	[Range(0.0F, 6.0F)]
 	public float UserVoltage;
 
-	private float[] voltages = {0.0F, 0.0F, 0.0F, 0.0F};
-	
-	private Motor PittmanMotor = new Motor(0.007F, 0.005F, 1.9F);
-	private float ThrustCoefficient = 0.0000005F;
+	//This array holds the Front Left, Front Right, Back Right and Back Left motor classes in that order
+	//Each motor has the same characteristics and constants
+	private Motor[] Motors = {
+		new Motor(0.007F, 0.005F, 1.9F),
+		new Motor(0.007F, 0.005F, 1.9F),
+		new Motor(0.007F, 0.005F, 1.9F),
+		new Motor(0.007F, 0.005F, 1.9F)
+	};
 
-    void Update() {
-		voltages = new float[] {UserVoltage, UserVoltage, UserVoltage, UserVoltage};
-		Motor[] motors = {PittmanMotor, PittmanMotor, PittmanMotor, PittmanMotor};
-		ApplyDynamics(voltages, motors);
+	//This is a proportionality value for the lift, determined experimentally
+	private float ThrustCoefficient = 0.0001F;
+
+	//This is the typical density of air at 15 degrees celcius
+	private float AirDensity = 1.225F;
+
+	//This is the diameter of the rotors
+	private float RotorDiameter = 0.5094F;
+
+	//This function runs at the frequency of the physics engine
+    void FixedUpdate() {
+		//This loop iterates over all four motors and rotors
+		for(int i = 0; i < 4; i++) {
+			//The angular velocity of the current rotor is found
+			float angularVelocity = Rotors[i].angularVelocity.magnitude;
+
+			//The torque of the motor and the thrust of the rotor are calculated
+			Motors[i].Update(UserVoltage, angularVelocity);
+			float torqueMagnitude = Motors[i].GetTorque();
+			float thrustMagnitude = CalculateThrust(angularVelocity);
+
+			//These vectors represent the directions of the torque and the thrust of the rotor
+			Vector3 thrustVector = Rotors[i].transform.up;
+			Vector3 torqueVector = Rotors[i].transform.up;
+
+			//If the rotor rotates clockwise, then the torque vector must point downwards instead of up
+			if(i % 2 == 1) {
+				torqueVector = -Rotors[i].transform.up;
+			}
+
+			//The torque and thrust vectors are sent to the physics engine
+			Rotors[i].AddTorque(torqueMagnitude * torqueVector);
+			Rotors[i].AddForce(thrustMagnitude * thrustVector);
+		}
     }
 
-	void ApplyDynamics(float[] voltages, Motor[] motors) {
-		for(int i = 0; i < 4; i++) {
-			float angularVelocity = 10 * Blades[i].angularVelocity.magnitude;
-			float torque = motors[i].CalculateTorque(voltages[i], angularVelocity);
-			float thrust = ThrustCoefficient * Mathf.Pow(angularVelocity, 2);
-			Vector3 thrustVector = Blades[i].transform.up;
-			Vector3 torqueVector = Blades[i].transform.up;
-			if(i % 2 == 1) {
-				torqueVector = -Blades[i].transform.up;
-			}
-			Blades[i].AddTorque(torque * torqueVector);
-			Blades[i].AddForce(thrust * thrustVector);
-		}
+	//This function applies the aerodynamic lift equation of a propeller to calculate thrust
+	float CalculateThrust(float angularVelocity) {
+		return ThrustCoefficient * AirDensity * Mathf.Pow(angularVelocity, 2) * Mathf.Pow(RotorDiameter, 4);
 	}
 
 }
